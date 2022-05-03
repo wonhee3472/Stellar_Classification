19369b81100e41f1a9e634071f70088577d2d7c7
Own Hee Yun <ownheey@wolfram.com>
2022-05-02 21:17:50 -0500

    adding changes to web page

--- Dashboard/js/quiz.js
+++ Dashboard/js/quiz.js
@@ -2,8 +2,8 @@ function check() {
     var question1 = document.quiz.question1.value;
     var question2 = document.quiz.question2.value;
     var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question3.value;
-    var question5 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
 
     var correct = 0;
 
